52690bf2ac6fab756748744715469d03ade3c61e
rqfvitor@gmail.com <rqfvitor@gmail.com>
2023-10-11 11:09:58 -0300

    feat: add relationships to Disk model

--- app/core/sebo/admin.py
+++ app/core/sebo/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
-from .models import Disk
+from .models import Disk, Seal, Artist
 
 admin.site.register(Disk)
+admin.site.register(Seal)
+admin.site.register(Artist)
--- app/core/sebo/models.py
+++ app/core/sebo/models.py
@@ -4,11 +4,24 @@ from django.db import models
 class Disk(models.Model):
     name = models.CharField(max_length=200)
     description = models.TextField()
-    seal = models.CharField(max_length=200)
+    seal = models.ForeignKey('Seal', on_delete=models.CASCADE)
     year = models.PositiveIntegerField()
     gender = models.CharField(max_length=200)
     country = models.CharField(max_length=200)
     quantity = models.PositiveIntegerField()
+    artists = models.ManyToManyField('Artist')
 
+    def __str__(self) -> str:
+        return self.name
+    
+class Seal(models.Model):
+    name = models.CharField(max_length=200)
+    
+    def __str__(self) -> str:
+        return self.name
+    
+class Artist(models.Model):
+    name = models.CharField(max_length=200)
+    
     def __str__(self) -> str:
         return self.name
